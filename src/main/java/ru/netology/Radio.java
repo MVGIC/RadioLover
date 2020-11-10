90ded6d97876cb4fd9e4f354e86b5900dfcd434e
Николай <ex3ct@mail.ru>
2020-11-10 14:13:30 +0700

    added methods and tests

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,15 +1,61 @@
 package ru.netology;
 
 public class Radio {
-    private int currentRadioStation = 10;
+    private int amountRadioStation = 10;
+    private int currentRadioStation;
     private int currentVolume;
 
     public Radio() {
     }
 
-    public Radio(int currentRadioStation, int currentVolume) {
+    public Radio(int amountRadioStation, int currentRadioStation) {
+        this.amountRadioStation = amountRadioStation;
         this.currentRadioStation = currentRadioStation;
+    }
+
+    public Radio(int currentVolume) {
         this.currentVolume = currentVolume;
+    }
+
+    public void nextRadioStation() {
+        if (currentRadioStation == amountRadioStation) {
+            this.currentRadioStation = 0;
+            return;
+        }
+        this.currentRadioStation = currentRadioStation + 1;
+    }
+
+    public void prevRadioStation() {
+
+        if (currentRadioStation == 0) {
+            this.currentRadioStation = amountRadioStation;
+            return;
+        }
+        this.currentRadioStation = currentRadioStation - 1;
+    }
+
+
+    public void minusVolume() {
+        if (currentVolume <= 0) {
+            return;
+        }
+        this.currentVolume = currentVolume - 1;
+    }
+
+
+    public void plusVolume() {
+        if (currentVolume >= 100) {
+            return;
+        }
+        this.currentVolume = currentVolume + 1;
+    }
+
+    public int getAmountRadioStation() {
+        return amountRadioStation;
+    }
+
+    public void setAmountRadioStation(int amountRadioStation) {
+        this.amountRadioStation = amountRadioStation;
     }
 
     public int getCurrentRadioStation() {
@@ -17,6 +63,11 @@ public class Radio {
     }
 
     public void setCurrentRadioStation(int currentRadioStation) {
+        if (currentRadioStation < 0) {
+            if (currentRadioStation > amountRadioStation){
+                return;
+            }
+        }
         this.currentRadioStation = currentRadioStation;
     }
 
--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -6,15 +6,77 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
     @Test
-    public void shouldUseConstructor() {
-        Radio radio = new Radio(20,50);
-        assertEquals(20,radio.getCurrentRadioStation());
-        assertEquals(50,radio.getCurrentVolume());
+    public void stationConstructor() {
+        Radio radio = new Radio(20,10);
+        assertEquals(20,radio.getAmountRadioStation());
+        assertEquals(10,radio.getCurrentRadioStation());
     }
 
     @Test
     public void shouldUseNoArgsConstructor(){
         Radio radio = new Radio();
-        assertEquals(10,radio.getCurrentRadioStation());
+        assertEquals(10,radio.getAmountRadioStation());
+    }
+
+    @Test
+    public void volumeConstructor(){
+        Radio radio = new Radio (50);
+        assertEquals(50,radio.getCurrentVolume());
+    }
+
+    @Test
+    void nextRadioStation() {
+        Radio radio = new Radio(20,10);
+        radio.nextRadioStation();
+        assertEquals(11, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    void prevRadioStation() {
+        Radio radio = new Radio(20,10);
+        radio.prevRadioStation();
+        assertEquals(9, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    void radioStationOverMax(){
+        Radio radio = new Radio(20,20);
+        radio.nextRadioStation();
+        assertEquals(0, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    void radioStationUnderMax(){
+        Radio radio = new Radio(20,0);
+        radio.prevRadioStation();
+        assertEquals(20, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    void minusVolume() {
+        Radio radio = new Radio(50);
+        radio.minusVolume();
+        assertEquals(49, radio.getCurrentVolume());
+    }
+
+    @Test
+    void plusVolume() {
+        Radio radio = new Radio(50);
+        radio.plusVolume();
+        assertEquals(51, radio.getCurrentVolume());
+    }
+
+    @Test
+    void plusVolumeOverMax(){
+        Radio radio = new Radio(100);
+        radio.plusVolume();
+        assertEquals(100, radio.getCurrentVolume());
+    }
+
+    @Test
+    void minusVolumeUnderMin(){
+        Radio radio = new Radio(0);
+        radio.minusVolume();
+        assertEquals(0, radio.getCurrentVolume());
     }
 }
